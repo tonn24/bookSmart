05ae9d7112df268fbcb890f3760cef127e4dc221
Tõnn <tonn.toomsalu@gmail.com>
2019-09-02 12:47:56 +0300

    add books.ejs, header,ejs, footer.ejs

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const express = require("express"),
       bodyParser = require("body-parser"),
       PORT = 9000;
 
+app.set("view engine", "ejs");
 
 
 app.get("/", (req, res) => {
-    res.send("landing page");
+    res.render("landing");
+});
+
+app.get("/books", (req, res) => {
+     var books = [
+         {title: "Sapiens: A Brief History of Humankind", image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1420585954l/23692271.jpg"},
+         {title: "Homo Deus: A History of Tomorrow", image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1468760805l/31138556._SY475_.jpg"}
+     ]
+    res.render("books", {books: books});
 });
 
 app.listen(PORT, () => {
